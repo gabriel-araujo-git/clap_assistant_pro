96d1ce4d86a2e63ee02f50581f887b950c144483
Gabriel Araujo <86240279+gabriel-araujo-git@users.noreply.github.com>
2025-11-21 08:31:55 -0300

    Delete src/web_commands.py

--- src/web_commands.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-def search_google(query: str):
-    webbrowser.open(f"https://www.google.com/search?q={query}")
-    return f"Pesquisando por: {query}"
-
-COMMANDS = {
-    "google": lambda: search_google(input("O que deseja pesquisar? "))
-}
